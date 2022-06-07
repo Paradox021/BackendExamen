480f72d05e2b147deede2814e3afeeca2f219645
paradox021 <espanacarmelo@gmail.com>
2022-06-07 12:49:45 +0200

    sin datos

--- src/main/java/com/example/demo/utils/Utils.java
+++ src/main/java/com/example/demo/utils/Utils.java
@@ -3,18 +3,7 @@ package com.example.demo.utils;
 
 public class Utils {
 
-    public static int[] contar(String cad){
-        String vocales = "aeiou";
-        String consonantes = "bcdfghjklmnñpqrstvwxyz";
-        int[] num = {0, 0};
-        int cadLength = cad.length();
-        for(int i = 0; i<cadLength; i++){
-            String letra = Character.toString(cad.charAt(i));
-            if(vocales.contains(letra)) num[0]++;
-            if(consonantes.contains(letra)) num[1]++;
-        }
-        return num;
-    }
+    
 
 
     public static String validaLetra(String cad){
@@ -52,13 +41,4 @@ public class Utils {
         return result;
     }
 
-    public static String transformaTexto(String texto){
-        char[] cadena = texto.toCharArray();
-        for(int i=0; i<cadena.length; i++){
-            if(Math.random()<0.5) cadena[i] = Character.toUpperCase(cadena[i]);      
-            else cadena[i] = Character.toLowerCase(cadena[i]);       
-        }
-        
-        return new String(cadena);
-    }
 }
